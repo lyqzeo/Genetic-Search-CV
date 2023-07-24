dc82b34e48b80c27cb8a39b114ad4e8eeb26a581
Zoe Lua <101164063+lyqzeo@users.noreply.github.com>
2023-07-24 09:18:02 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
 3
+
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+    def __lt__(self, other):
+        # Define custom behavior for "<"
+        return self.value < other.value
+
+    def __le__(self, other):
+        # Define custom behavior for "<="
+        return self.value <= other.value
+
+    def __ge__(self, other):
+        # Define custom behavior for ">="
+        return self.value >= other.value
+
+    def __eq__(self, other):
+        # Define custom behavior for "=="
+        return self.value == other.value
+
+    def __ne__(self, other):
+        # Define custom behavior for "!="
+        return self.value != other.value
